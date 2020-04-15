3aad121dfe307943bec1b58caf6df7065ce1716f
ptolomaeus <ptolomaeus89@gmail.com>
2020-04-15 19:13:20 +0900

    init week2

--- anyint/solver.py
+++ anyint/solver.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os
+from subprocess import Popen, PIPE
+
 def solve_it(input_data):
-    # return a positive integer, as a string
-    return '0'
+
+    # Runs the command: java Solver -file=tmp.data
+
+    process = Popen(['../cpp_solver/anyint'], stdout=PIPE, universal_newlines=True)
+    (stdout, stderr) = process.communicate()
+
+    return stdout.strip()
 
 if __name__ == '__main__':
     print('This script submits the integer: %s\n' % solve_it(''))
+    # print('This script submits the integer: %s\n' % solve_it(''))
 
